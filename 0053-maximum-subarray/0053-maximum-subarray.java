bc14586b69c8f33a1018e21d7bdbee718779b041
vtu25932-pardhu <vtu25932@veltech.edu.in>
2026-02-19 09:23:17 +0530

    Time: 1 ms (99.67%), Space: 80.8 MB (5.49%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSoFar = Math.max(maxSoFar, currentSum);
+        }
+
+        return maxSoFar;
+    }
+}
