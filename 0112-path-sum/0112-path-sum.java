beb1482a4236fcb4228982369a48a4bdbdf78af3
vtu25932-pardhu <vtu25932@veltech.edu.in>
2026-03-27 09:21:09 +0530

    Time: 0 ms (100%), Space: 45.1 MB (41.04%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        return hasPathSum(root.left, targetSum - root.val) ||
+               hasPathSum(root.right, targetSum - root.val);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
